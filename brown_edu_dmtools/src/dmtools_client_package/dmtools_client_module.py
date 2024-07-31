6b05a04ba10bd9654132c0bccd156522c6957b04
andrew gaitskell <andrew@gaitskell.com>
2024-07-31 20:10:45 +0100

    x

--- brown_edu_dmtools/src/dmtools_client_package/dmtools_client_module.py
+++ brown_edu_dmtools/src/dmtools_client_package/dmtools_client_module.py
@@ -84,7 +84,11 @@ class DMToolsClient():
 
         # Create the request object
         req = urllib.request.Request(self.current_url, data=json_data, headers={'Content-Type': 'application/json'}, method='POST')
-
+        
+        with urllib.request.urlopen(req) as response:
+                response_data = response.read().decode('utf-8')
+                return response.status, response_data
+        '''
         try:
             # Send the request and get the response
             with urllib.request.urlopen(req) as response:
@@ -101,6 +105,7 @@ class DMToolsClient():
                 return e.code, f"HTTP Error {e.code}: {e.reason}\nError message: {error_message}"
         except urllib.error.URLError as e:
             return None, f"URL Error: {e.reason}"
+        '''
 
     '''
     def create_current(self,data_in,url_in): ## leave url in as needed for not found error
