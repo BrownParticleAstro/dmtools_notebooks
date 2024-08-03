2efb8267501ce7e712f7afb50e93f38545de15ff
andrew gaitskell <andrew@gaitskell.com>
2024-08-03 10:12:24 +0100

    deleted library

--- brown_edu_dmtools/src/package_b/module_b.py
+++
@@ -1,3 +0,0 @@
-def add_two(number):
-    return number + 2
-
--- brown_edu_dmtools/tests/test_module_b.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from package_b.module_b import add_two 
-
-class TestAddTwo(unittest.TestCase):
-  	def test_add_two(self):
-  		  self.assertEqual(add_two(1), 3)
-
-unittest.main()
